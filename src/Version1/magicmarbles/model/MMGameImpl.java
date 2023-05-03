c44d05c8a1041f4863b788b20d7dfd878f8aebbd
Nikodus <niklas.loidls@gmail.com>
2023-05-03 13:42:07 +0200

    Version 1 Added Files

---
+++ src/Version1/magicmarbles/model/MMGameImpl.java
@@ -0,0 +1,53 @@
+package magicmarbles.model;
+
+/**
+ * Implementation of the magic marbles game
+  */
+public class MMGameImpl implements MMGame {
+
+	/**
+	 * Constructor 
+	 * @param width the width of the game board
+	 * @param height the height of the game board
+	 */
+	public MMGameImpl(int width, int height) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public MMState getGameState() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getGamePoints() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public MMFieldState getFieldState(int row, int col) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void select(int row, int col) throws MMException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
