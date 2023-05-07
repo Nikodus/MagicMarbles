7d72d5ba649822e72115431aa5a10be98ae2bdaa
Nikodus <niklas@loidls.com>
2023-05-07 16:27:41 +0200

    Version 1 HELP EVEN MORE

--- src/Version1/magicmarbles/model/MMGameImpl.java
+++ src/Version1/magicmarbles/model/MMGameImpl.java
@@ -1,6 +1,13 @@
 package Version1.magicmarbles.model;
 
+import Version1.inout.In;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * Implementation of the magic marbles game
@@ -16,6 +23,7 @@ public class MMGameImpl implements MMGame {
 	private final int height;
 	private int score;
 	private int[][] Gamefield;
+	private boolean win = false;
 
 
 
@@ -23,9 +31,9 @@ public class MMGameImpl implements MMGame {
 
 		this.width = width;
 		this.height = height;
-		this.Gamefield = new int[width][height];
+		this.Gamefield = new int[height][width];
 		Random rand = new Random();
-		for (int y = 0; y < Gamefield[1].length; y++) {
+		for (int y = 0; y < Gamefield[0].length; y++) {
 			for (int x = 0; x < Gamefield.length; x++) {
 				this.Gamefield[x][y] = rand.nextInt(3);
 			}
@@ -48,7 +56,8 @@ public class MMGameImpl implements MMGame {
 
 	@Override
 	public MMState getGameState() {
-		// TODO Auto-generated method stub
+
+		if(win)return MMState.END;
 		return MMState.RUNNING;
 	}
 
@@ -62,18 +71,18 @@ public class MMGameImpl implements MMGame {
 	public MMFieldState getFieldState(int row, int col) {
 
 
-		if(Gamefield[col][row] == 0)return MMFieldState.RED;
-		if(Gamefield[col][row] == 1)return MMFieldState.GREEN;
-		if(Gamefield[col][row] == 2)return MMFieldState.BLUE;
+		if(Gamefield[row][col] == 0)return MMFieldState.RED;
+		if(Gamefield[row][col] == 1)return MMFieldState.GREEN;
+		if(Gamefield[row][col] == 2)return MMFieldState.BLUE;
 
 		return MMFieldState.EMPTY;
 	}
 
 	public void setFieldState(int row, int col, MMFieldState state) {
-		if(state == MMFieldState.RED) Gamefield[col][row] = 0;
-		if(state == MMFieldState.GREEN) Gamefield[col][row] = 1;
-		if(state == MMFieldState.BLUE) Gamefield[col][row] = 2;
-		if(state == MMFieldState.EMPTY) Gamefield[col][row] = 3;
+		if(state == MMFieldState.RED) Gamefield[row][col] = 0;
+		if(state == MMFieldState.GREEN) Gamefield[row][col] = 1;
+		if(state == MMFieldState.BLUE) Gamefield[row][col] = 2;
+		if(state == MMFieldState.EMPTY) Gamefield[row][col] = 3;
 
 
 	}
@@ -82,19 +91,24 @@ public class MMGameImpl implements MMGame {
 
 	@Override
 	public void select(int row, int col) throws MMException {
+		if(getFieldState(row, col) == MMFieldState.EMPTY) throw new MMException();
 		MMFieldClear clear = new MMFieldClear(MMFieldState.EMPTY,row,col);
-		clear.moveEmpty(row, col);
+		clear.moveEmpty();
+		win = clear.checkWin();
+		score = score + (clear.getClearedfields() * clear.getClearedfields());
 	}
 
 
 	private class MMFieldClear {
 
 		private MMFieldState colorstate;
+		private int clearedfields;
 
 		public MMFieldClear(MMFieldState colorstate, int row, int col) throws MMException {
 			if(getFieldState(row, col) != MMFieldState.EMPTY)
 			{
 				this.colorstate = colorstate;
+				clearedfields = 1;
 				clearField(row, col);
 
 			}
@@ -102,7 +116,8 @@ public class MMGameImpl implements MMGame {
 
 		}
 
-		public void clearField(int row, int col) throws MMException {
+		public void clearField(int row, int col){
+
 
 			try {
 				if ((getFieldState(row, col) == getFieldState(row - 1, col) || colorstate == getFieldState(row - 1, col)) && getFieldState(row - 1, col) != MMFieldState.EMPTY) {
@@ -110,6 +125,7 @@ public class MMGameImpl implements MMGame {
 					clearField(row - 1, col);
 					if(getFieldState(row-1, col) != MMFieldState.EMPTY)this.colorstate = getFieldState(row-1, col);
 					setFieldState(row-1, col, MMFieldState.EMPTY);
+					clearedfields++;
 				}
 			}
 			catch (IndexOutOfBoundsException e){};
@@ -120,6 +136,7 @@ public class MMGameImpl implements MMGame {
 					clearField(row,col-1);
 					if(getFieldState(row, col-1) != MMFieldState.EMPTY)this.colorstate = getFieldState(row, col-1);
 					setFieldState(row,col-1,MMFieldState.EMPTY);
+					clearedfields++;
 				}
 			}catch (IndexOutOfBoundsException e){};
 			try {
@@ -129,6 +146,7 @@ public class MMGameImpl implements MMGame {
 					clearField(row+1,col);
 					if(getFieldState(row+1, col) != MMFieldState.EMPTY)this.colorstate = getFieldState(row+1, col);
 					setFieldState(row+1,col,MMFieldState.EMPTY);
+					clearedfields++;
 				}
 			}catch (IndexOutOfBoundsException e){};
 			try {
@@ -138,26 +156,84 @@ public class MMGameImpl implements MMGame {
 					clearField(row,col+1);
 					if(getFieldState(row, col+1) != MMFieldState.EMPTY)this.colorstate = getFieldState(row, col+1);
 					setFieldState(row,col+1,MMFieldState.EMPTY);
+					clearedfields++;
 				}
 			}catch (IndexOutOfBoundsException e){};
 
 
 		}
-		public void moveEmpty(int row, int col) throws MMException {
-			for (int x = Gamefield.length-1; x >0 ; x--) {
-				for (int y = Gamefield[1].length-1, count = 0; y > 0 ; y--) {
-					if(Gamefield[x][y] == 3)count++;
-					for (int i = 0; i < count; i++) {
-						for (int z = 0; Gamefield[1].length-1 > z ; z++) {
-							int save = Gamefield[x][z];
-							Gamefield[x][z] = Gamefield[x][z+1];
-							Gamefield[x][z+1] = save;
+
+		//	3x4 Field
+		//				 x-Axis  <-> (column) (Width)
+		//				   0  1  2  3
+		//	^			0  r  g  b  r
+		//	|	y-Axis	1  b  r  r  g
+		//	V	(row)   2  g  g  b  b
+		//		(Height)
+		//
+
+		public void moveEmpty(){
+			for (int x = 0; x < Gamefield[0].length; x++) {
+				int count = getHeight();
+				for (int y = 0; y < Gamefield.length; y++) {
+					if (Gamefield[y][x] == 3) count--;
+				}
+				for (; count>0;count--) {
+					for(int i = 1; i < Gamefield.length; i++)
+					{
+						if(Gamefield[i][x] == 3)
+						{
+							setFieldState(i,x,getFieldState(i-1,x));
+							setFieldState(i-1,x,MMFieldState.EMPTY);
 						}
 					}
-
 				}
 			}
 
+			ArrayList<ArrayList<Integer>> rightList = new ArrayList<ArrayList<Integer>>();
+
+
+			for (int x = 0; x < Gamefield[0].length; x++) {
+				ArrayList<Integer> temprightList = new ArrayList<Integer>();
+				for (int y = 0; y < Gamefield.length; y++) {
+					temprightList.add(Gamefield[y][x]);
+				}
+				rightList.add(temprightList);
+			}
+
+			for (int i = 0; i < rightList.size(); i++) {
+				if(rightList.get(i).get(rightList.get(i).size()-1) == 3)
+				{
+					ArrayList<Integer> DummyList = new ArrayList<Integer>(rightList.get(i));
+					rightList.remove(i);
+					rightList.add(0,DummyList);
+				}
+			}
+
+			for (int x = 0; x < rightList.size(); x++) {
+				for (int y = 0; y < rightList.get(0).size(); y++) {
+					Gamefield[y][x] = rightList.get(x).get(y);
+				}
+			}
+		}
+
+		public boolean checkWin() throws MMException {
+			{
+				boolean win = true;
+				for (int x = 0; x < Gamefield.length; x++) {
+					for (int y = 0; y < Gamefield[0].length; y++) {
+						if(getFieldState(y,x) != MMFieldState.EMPTY)
+						{
+							win = false;
+						}
+					}
+				}
+				return win;
+			}
+		}
+
+		public int getClearedfields() {
+			return clearedfields;
 		}
 	}
 }
